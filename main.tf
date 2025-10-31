75319d50d0cc80824ad3dc5a51d499cdc8842735
Saiteja7120 <saitejavempatapu11@gmail.com>
2025-10-31 15:40:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_s3_bucket" "terraform_bucket" {
   force_destroy = true
  
   tags = {
-    Name        = "Terraforms33"
+    Name        = "teJS1381839"
     Environment = "Dev"
   }
 }    
